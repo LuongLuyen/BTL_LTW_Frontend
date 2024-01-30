0a1855152cc0783d8a335d3d04c5eb3b22adf2e5
LuongLuyen <luongvanluyen2003@gmail.com>
2024-01-30 11:59:50 +0700

    config router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
